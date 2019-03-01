1655f12ba6718e9bc1091be6356fc3950c73cfd7
Alex Walters <alexnwalters@gmail.com>
2019-03-01 01:17:42 -0500

    change navigation to scroll

--- script.js
+++ script.js
@@ -1,66 +1,3 @@
-function toggleHideHome() {
-    $('main').find('.home').toggleClass('js-hide', false);
-    $('main').find('.about').toggleClass('js-hide', true);
-    $('main').find('.projects').toggleClass('js-hide', true);
-    $('main').find('.contact').toggleClass('js-hide', true);
-    console.log("ran toggleHideHome");
-}
-
-function toggleHideAbout() {
-    $('main').find('.home').toggleClass('js-hide', true);
-    $('main').find('.about').toggleClass('js-hide', false);
-    $('main').find('.projects').toggleClass('js-hide', true);
-    $('main').find('.contact').toggleClass('js-hide', true);
-    console.log("ran toggleHideAbout");
-}
-
-function toggleHideProjects() {
-    $('main').find('.home').toggleClass('js-hide', true);
-    $('main').find('.about').toggleClass('js-hide', true);
-    $('main').find('.projects').toggleClass('js-hide', false);
-    $('main').find('.contact').toggleClass('js-hide', true);
-    console.log("ran toggleHideProjects");
-}
-
-function toggleHideContact() {
-    $('main').find('.home').toggleClass('js-hide', true);
-    $('main').find('.about').toggleClass('js-hide', true);
-    $('main').find('.projects').toggleClass('js-hide', true);
-    $('main').find('.contact').toggleClass('js-hide', false);
-    console.log("ran toggleHideHome");
-}
-
-function handleHomeTab() {
-    $('.nav-list').on('click', '#home', function(event) {
-    console.log("ran handleHomeTab");
-    toggleHideHome();
-    location.reload();
-    });
-}
-
-function handleAboutTab() {
-    $('.nav-list').on('click', '#about', function(event) {
-    console.log("ran handleAboutTab");  
-    toggleHideAbout();
-    openHideMenu();
-    });
-}
-
-function handleProjectsTab() {
-    $('.nav-list').on('click', '#projects', function(event) {
-    console.log("ran handleProjectsTab");  
-    toggleHideProjects();
-    openHideMenu();
-    });
-}
-
-function handleContactTab() {
-    $('.nav-list').on('click', '#contact', function(event) {
-    console.log("ran handleContactTab");  
-    toggleHideContact();
-    openHideMenu();
-    });
-}
 
 function openHideMenu() {
     $('nav').find('.nav-list').toggleClass('js-menu');
@@ -68,24 +5,19 @@ function openHideMenu() {
 
 function handleHamburgerMenu() {
     $('.navbar').on('click', '.hamburger', function(event) {
-        console.log("ran handleHamburgerMenu");
         openHideMenu();
     });
 }
 
-function handleIconClick() {
-    $('.navbar').on('click', '.icon', function(event) {
-        location.reload();
-    })
+function handleSectionClick() {
+    $('.nav-list').on('click', 'a', function(event) {
+    openHideMenu();
+    });
 }
 
 function runPortfolio() {
-    handleHomeTab();
-    handleAboutTab();
-    handleProjectsTab();
-    handleContactTab();
     handleHamburgerMenu();
-    handleIconClick();
+    handleSectionClick();
 }
 
 $(runPortfolio);
